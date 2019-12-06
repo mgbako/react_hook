1a737ed81b6bf6a241fc307101e9e41a09abc4c5
mgbakocentric <jmgbako@gmail.com>
2019-12-06 03:45:14 +0100

    Created the project and added react hook for form and fetching of data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Todos({ todo, index }) {
+	return <div className="todo">{todo.text}</div>;
+}
+
+function TodoForm({ addTodo }) {
+	const [ value, setValue ] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!value) return;
+
+		addTodo(value);
+		setValue('');
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				className="input"
+				placeholder="Add todo."
+				autoFocus
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</form>
+	);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ todos, setTodos ] = useState([
+		{ text: 'Learn about Reactjs', isCompleted: false },
+		{ text: 'Learn about Angular', isCompleted: false },
+		{ text: 'Learn about Vuejs', isCompleted: false },
+		{ text: 'Learn about Ionicjs', isCompleted: false }
+	]);
+
+	const addTodo = (text) => {
+		const newTodo = [ ...todos, { text } ];
+		setTodos(newTodo);
+	};
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => <Todos index={index} key={index} todo={todo} />)}
+				<TodoForm addTodo={addTodo} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
